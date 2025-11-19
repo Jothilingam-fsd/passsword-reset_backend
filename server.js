a9a0c259e79881fb35018f4e2d7c095def8fe69d
jothilingam-fsd <jothilingamfsd@gmail.com>
2025-11-19 10:21:04 +0530

    Fix MongoDB URI and server.js

--- server.js
+++ server.js
@@ -54,6 +54,10 @@ connectDB();
 // Start server
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
